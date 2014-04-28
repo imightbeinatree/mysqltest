7e5e6547512fba562dc096f988787017808f1ec9
Michael Orr <michael@cloudspace.com>
2014-04-28 13:01:32 -0400

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Mysqltest::Application.config.secret_key_base = 'e63a2f872179792d5300f4710476f4f52b67d00274c0504dcc7d322c084904a5ed56993c9a17b91c080bcde23a63522c604d5c2c7d86a98fd0a2fc57c254bf16'
